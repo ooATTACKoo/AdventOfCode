5ddf481e6f5d1c12bcc61e7d0a55a28785c3cd3c
Christoph Ackermann <Christoph.Ackermann@simcorp.com>
2025-12-02 11:18:10 +0100

    Day2 is finished

--- 2024/AdventOfCode2024/MyAdventTests/AdventOfCode2025_1-9.cs
+++ 2024/AdventOfCode2024/MyAdventTests/AdventOfCode2025_1-9.cs
@@ -65,7 +65,7 @@ namespace MyAdventTests
         char direction = line[0];
         int number = int.Parse(line.Substring(1));
         int oldpos = startpos;
-        if ( number > 99)
+        if (number > 99)
         {
           Console.WriteLine(number);
 
@@ -74,11 +74,12 @@ namespace MyAdventTests
         {
           startpos = (startpos + number);
           while (startpos > 99)
-          { startpos = startpos - 100;
+          {
+            startpos = startpos - 100;
             password++;
           }
         } else
-        { 
+        {
           if (oldpos == 0)
           {
             password--;
@@ -95,6 +96,98 @@ namespace MyAdventTests
         }
       }
       Assert.AreEqual(7199, password);
+    }
+
+
+    [TestMethod]
+    public void Day2a()
+    {
+      string data = FileReader.LoadFileIntoOneString("2025/02A.txt");
+      data = data.Replace("\r\n", "").Replace("\n", "");
+      List<string> ranges = data.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+      long result = 0;
+      foreach (string range in ranges)
+      {
+        List<string> numbers = range.Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        string start = numbers.First();
+        string end = numbers.Last();
+
+        long startint = long.Parse(start);
+        long endint = long.Parse(end);
+        for (long i = startint; i <= endint; i++)
+        {
+          string checknumber = i.ToString();
+          if (checknumber.Length % 2 == 1)
+          { continue; }
+
+          string frontnumber = checknumber.Substring(0, checknumber.Length / 2);
+          string endnumber = checknumber.Substring(checknumber.Length / 2, checknumber.Length / 2);
+
+          if (frontnumber == endnumber)
+          {
+            result += i;
+          }
+
+        }
+      }
+      Assert.AreEqual(result, 31000881061);
+    }
+
+
+    [TestMethod]
+    public void Day2b()
+    {
+      string data = FileReader.LoadFileIntoOneString("2025/02A.txt");
+      data = data.Replace("\r\n", "").Replace("\n", "");
+      List<string> ranges = data.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+      long result = 0;
+      foreach (string range in ranges)
+      {
+        List<string> numbers = range.Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        string start = numbers.First();
+        string end = numbers.Last();
+
+        long startint = long.Parse(start);
+        long endint = long.Parse(end);
+        for (long i = startint; i <= endint; i++)
+        {
+          string checknumber = i.ToString();
+          result = IdFinderByString(result, i, checknumber);
+        }
+
+      }
+      Assert.AreEqual(result, 46769308485);
+    }
+
+    private static long IdFinderByString(long result, long i, string checknumber)
+    {
+      for (int splitpos = 1; splitpos <= checknumber.Length / 2; splitpos++)
+      {
+        if (checknumber.Length % splitpos != 0)
+        {
+          continue;
+        }
+
+        string frontnumber = checknumber.Substring(0, splitpos);
+        bool isId = true;
+        for (int pos = splitpos; pos < checknumber.Length; pos += splitpos)
+        {
+          string nextpart = checknumber.Substring(pos, splitpos);
+          if (frontnumber != nextpart)
+          {
+            isId = false;
+            break;
+          }
+        }
+
+        if (isId)
+        {
+          result += i;
+          break;
+        }
+      }
+
+      return result;
     }
   }
 }
