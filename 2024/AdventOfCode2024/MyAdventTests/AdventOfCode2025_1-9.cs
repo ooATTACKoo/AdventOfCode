be38540af10ebb42c2d7db62e3a8d50262b1bd23
Christoph Ackermann <Christoph.Ackermann@simcorp.com>
2025-12-04 10:51:05 +0100

    move visualizer into a seperate class

--- 2024/AdventOfCode2024/MyAdventTests/AdventOfCode2025_1-9.cs
+++ 2024/AdventOfCode2024/MyAdventTests/AdventOfCode2025_1-9.cs
@@ -10,7 +10,7 @@ namespace MyAdventTests
 
 {
   [TestClass]
-  public class AdventOfCode2025
+  public class AdventOfCode2025 : Visualizer
   {
 
     [TestCategory("L0")]
@@ -295,41 +295,6 @@ namespace MyAdventTests
         File.Delete(file);
       }
       Assert.AreEqual(8936, counter);
-    }
-
-    void SaveMatrixAsImage(List<List<char>> matrix, string filename)
-    {
-      int cellSize = 10;
-      int width = matrix[0].Count * cellSize;
-      int height = matrix.Count * cellSize;
-      using (var bmp = new Bitmap(width, height))
-      using (var g = Graphics.FromImage(bmp))
-      {
-        g.Clear(Color.White);
-        for (int row = 0; row < matrix.Count; row++)
-        {
-          for (int col = 0; col < matrix[row].Count; col++)
-          {
-            Color color = matrix[row][col] == '@' ? Color.Yellow : Color.Blue;
-            g.FillRectangle(new SolidBrush(color), (col * cellSize)+1, (row * cellSize)+1, cellSize -2, cellSize-2);
-          }
-        }
-        bmp.Save(filename);
-      }
-    }
-
-    void CreateGifFromImages(string[] imageFiles, string outputGif)
-    {
-      using (var collection = new MagickImageCollection())
-      {
-        foreach (var file in imageFiles)
-        {
-          var image = new MagickImage(file);
-          image.AnimationDelay = 3; // 10 = 0.1s per frame
-          collection.Add(image);
-        }
-        collection.Write(outputGif);
-      }
     }
 
     List<(int row, int col)> directions = new List<(int row, int col)>
---
+++ 2024/AdventOfCode2024/MyAdventTests/Visualizer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyAdventTests
+{
+  public static class Visualizer
+  {
+
+    public static void CreateGifFromImages(string[] imageFiles, string outputGif)
+    {
+      using (var collection = new MagickImageCollection())
+      {
+        foreach (var file in imageFiles)
+        {
+          var image = new MagickImage(file);
+          image.AnimationDelay = 3; // 10 = 0.1s per frame
+          collection.Add(image);
+        }
+        collection.Write(outputGif);
+      }
+    }
+
+    public static void SaveMatrixAsImage(List<List<char>> matrix, string filename)
+    {
+      int cellSize = 10;
+      int width = matrix[0].Count * cellSize;
+      int height = matrix.Count * cellSize;
+      using (var bmp = new Bitmap(width, height))
+      using (var g = Graphics.FromImage(bmp))
+      {
+        g.Clear(Color.White);
+        for (int row = 0; row < matrix.Count; row++)
+        {
+          for (int col = 0; col < matrix[row].Count; col++)
+          {
+            Color color = matrix[row][col] == '@' ? Color.Yellow : Color.Blue;
+            g.FillRectangle(new SolidBrush(color), (col * cellSize) + 1, (row * cellSize) + 1, cellSize - 2, cellSize - 2);
+          }
+        }
+        bmp.Save(filename);
+      }
+    }
+  }
+}
